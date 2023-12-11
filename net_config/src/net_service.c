eef2ec4956b30442ed6faaa855ec5d69910eca31
xqhuang <1137512847@qq.com>
2023-12-11 21:23:13 +0800

    处理子进程退出没SIGCHDL信号、模块化和添加select处理多并发

--- net_config/src/net_service.c
+++ net_config/src/net_service.c
@@ -4,8 +4,42 @@
 #include "net_intf.h"
 #include "net_service.h"
 #include <signal.h>
+#include <sys/wait.h>
+#include <sys/select.h>
+/*
+@addr:服务端地址
+@port:服务端端口
+@return:成功返回sock描述
+*/
+int sock_service_init(const char *addr,unsigned int port)
+{
+        int sock_fd;
+        if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+                ERR_EXIT("clinet socket");
+        }
 
+        struct sockaddr_in sockaddr_service;
+        memset(&sockaddr_service, 0, sizeof(sockaddr_service));
+        sockaddr_service.sin_family = AF_INET;
+        sockaddr_service.sin_port = htons(port);
+        sockaddr_service.sin_addr.s_addr = inet_addr(addr) ;
 
+        if(bind(sock_fd, (struct sockaddr*)&sockaddr_service, sizeof(sockaddr_service)) < 0)  {
+                ERR_EXIT("clinet bind");
+        }
+
+        if(listen(sock_fd, SOMAXCONN) < 0) {
+                ERR_EXIT("clinet listen");
+        }
+	
+	return sock_fd;
+}
+
+/*
+ *服务端处理
+ *@conn:已连接套接字
+ *
+ */
 void do_service(int conn)
 {
         int data_len;
@@ -42,59 +76,113 @@ void do_service(int conn)
 
         }
 }
+/*
+ *多进程处理并发
+ *@listenfd：侦听套接字
+ *
+ */
+int sock_fork(int listenfd)
+{
+        int conn_fd;
+        pid_t client_pid;
+        struct sockaddr_in sockaddr_client;
+        socklen_t client_len = sizeof(sockaddr_client);
+        memset(&sockaddr_client, 0, sizeof(sockaddr_client));
 
-void head_fun(int sig)
+        while(1)
+        {
+                if((conn_fd = accept(listenfd, (struct sockaddr*)&sockaddr_client, &client_len)) < 0) {
+                        ERR_EXIT("clinet accept");
+                }
+
+                client_pid = fork();
+                if (client_pid < 0)
+                {
+                        ERR_EXIT("fork");
+                }else if (client_pid == 0) { //子进程
+
+                        do_service(conn_fd);
+                        close(conn_fd);
+                        exit(EXIT_SUCCESS);
+                }
+        }
+	return 0;
+}
+
+int sock_select()
+{
+	int nready;
+	int conn_fd;
+        pid_t client_pid;
+        struct sockaddr_in sockaddr_client;
+        socklen_t client_len = sizeof(sockaddr_client);
+        memset(&sockaddr_client, 0, sizeof(sockaddr_client));
+
+
+	int maxfd = listenfd;
+	fd_set allset;
+	fd_set rset;
+	FD_ZERO(&allset);
+	FD_ZERO(&rset);
+	FD_SET(listenfd,&allset)
+        
+	int i;
+        int client[FD_MAXSIZE];
+        for (i=0; i<FD_MAXSIZE; i++)
+                 client[FD_MAXSIZE] = -1;	
+	while(1)
+	{
+		rset = allset;
+		nready = select(maxfd+1, &rset, NULL, NULL, NULL);
+		if (FD_ISSET(listenfd,&rset))
+		{
+	                if((conn_fd = accept(listenfd, (struct sockaddr*)&sockaddr_client, &client_len)) < 0) {
+        	                ERR_EXIT("clinet accept");
+               		 }
+			
+			for (i=0; i<FD_MAXSIZE; i++){
+				if (client[i] < 0) {
+					client[i] = conn_fd;
+					if (conn_fd > maxfd)
+						maxfd = conn_fd;
+					break;
+				}
+			}
+			
+			if (i>=FD_MAXSIZE)
+			{
+				fprintf(stderr,"too may client.");
+				exit(EXIT,FAILURE);
+			}
+
+			FD_SET(client[i],&allset);
+
+			if (--nready <= 0)
+				continue;
+		}
+
+
+		if (FD_ISSET(listenfd,&rset))
+
+	}
+}
+
+void header_fun(int sig)
 {
 	printf("sig test\n");
+	while (waitpid(-1, NULL,WNOHANG) > 0);
 }
 
 int sock_service()
 {
 	signal(SIGCHLD,head_fun);
-        int sock_fd;
-        if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-                ERR_EXIT("clinet socket");
-        }
 
-        struct sockaddr_in sockaddr_service;
-        memset(&sockaddr_service, 0, sizeof(sockaddr_service));
-        sockaddr_service.sin_family = AF_INET;
-        sockaddr_service.sin_port = htons(12345);
-        sockaddr_service.sin_addr.s_addr = inet_addr("127.0.0.1") ;
 
-        if(bind(sock_fd, (struct sockaddr*)&sockaddr_service, sizeof(sockaddr_service)) < 0)  {
-                ERR_EXIT("clinet bind");
-        }
-
-        if(listen(sock_fd, SOMAXCONN) < 0) {
-                ERR_EXIT("clinet listen");
-        }
-
-        int conn_fd;
-        struct sockaddr_in sockaddr_client;
-        socklen_t client_len = sizeof(sockaddr_client);
-	memset(&sockaddr_client, 0, sizeof(sockaddr_client));
-	while(1) 
-	{
-        	if((conn_fd = accept(sock_fd, (struct sockaddr*)&sockaddr_client, &client_len)) < 0) {
-               		ERR_EXIT("clinet accept");
-        	}
-	
-		pid_t client_pid;
-       		client_pid = fork();
-		if (client_pid < 0)
-		{
-			ERR_EXIT("fork");
-		}else if (client_pid == 0) { //子进程
-		
-			do_service(conn_fd);
-			close(conn_fd);
-		}
-
-	}
-
-	close(sock_fd);
-        
+	int listenfd;
+	listenfd = sock_service_init("127.0.0.1",12345);
+	//(void)sock_fork(listenfd);
+	close(listenfd);
+   
 	return 0;
 
 }
